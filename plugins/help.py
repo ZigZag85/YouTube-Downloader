e29ba74a9f990e9f01fa5146712af81e1f7b5665
Mohamed Rizad <rizaddq789654@gmail.com>
2021-12-04 09:19:35 -0500

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -4,7 +4,7 @@ from pyrogram import Client, Filters
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
     helptxt = f"""Just Send Youtube Url And Select A Quality
-    For Download (it can download both audio & video Format)
+For Download (it can download both audio & video Format)
     
-         Queries [rizadッ](https://telegram.me/rizad_x96)"""
+Queries [rizadッ🕊](https://telegram.me/rizad_x96)"""
     await message.reply_text(helptxt)
